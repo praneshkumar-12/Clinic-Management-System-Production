1ee8f1a6dd21c5dd64ca6f01bc05d46babc5b6f7
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-06-19 23:33:28 +0530

    cahnged csrf trusted origin

--- ClinicManagementSystemProduction/settings.py
+++ ClinicManagementSystemProduction/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-CSRF_TRUSTED_ORIGINS = ["https://cms-django.azurewebsites.net", "https://www.cms-django.azurewebsites.net"]
+CSRF_TRUSTED_ORIGINS = ["https://cms-django-test.azurewebsites.net", "https://www.cms-django-test.azurewebsites.net"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
