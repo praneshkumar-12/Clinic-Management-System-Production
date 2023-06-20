f09c8eb0c727a13800b852e604b5efa5b7eea238
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-06-20 18:12:34 +0530

    changed debug

--- ClinicManagementSystemProduction/settings.py
+++ ClinicManagementSystemProduction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-z&k!@+m3*(fv0yy0+&*+_i05z4)6k%(8((^fnc+n4unw7$e2)3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
